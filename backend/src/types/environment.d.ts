930cd2faaca1e4f5554dca40d63363c1f046d7e3
JNCJcoder <jorgenettocjunior@gmail.com>
2022-11-16 16:22:22 -0300

    Adicionado Variável de ambiente JWT_SECRET

--- backend/src/types/environment.d.ts
+++ backend/src/types/environment.d.ts
@@ -1,9 +1,12 @@
+import type { Secret } from 'jsonwebtoken';
+
 declare global {
     namespace NodeJS {
         interface ProcessEnv {
             NODE_ENV: 'development' | 'production';
             PORT?: string;
             SALT: string;
+            JWT_SECRET: Secret;
         }
     }
 }
