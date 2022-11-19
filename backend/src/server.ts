b46b15cd3f581edbb1f4b9ba45845195b0e05d8b
JNCJcoder <jorgenettocjunior@gmail.com>
2022-11-19 17:59:28 -0300

    Create server.ts

---
+++ backend/src/server.ts
@@ -0,0 +1,33 @@
+import "reflect-metadata";
+import express from 'express';
+
+import myDataSource from "./app-data-source";
+import errorHandler from './middlewares/errorHandler';
+
+import userController from './controllers/UserController';
+import accountController from "./controllers/AccountController";
+import transactionController from "./controllers/TransactionController";
+
+
+myDataSource.initialize()
+    .then(() => {
+        console.log("Data Source iniciado.")
+    })
+    .catch((err) => {
+        console.error("Erro durante o inicio do Data Source:", err)
+    });
+
+const app = express();
+
+// Aplicação
+app.use(express.json());
+
+// Rotas
+app.use(userController);
+app.use(accountController);
+app.use(transactionController);
+
+// Error Handler
+app.use(errorHandler);
+
+export default app;
