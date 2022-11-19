5f410def6bd441db654d52e546886a7940c59afa
JNCJcoder <jorgenettocjunior@gmail.com>
2022-11-19 18:01:20 -0300

    Adicionado middleware para gerenciar erros

--- backend/src/middlewares/errorHandler.ts
+++ backend/src/middlewares/errorHandler.ts
@@ -1,14 +1,34 @@
 import type { Request, Response, NextFunction } from "express";
 import { StatusCodes } from "http-status-codes";
+
 import ForbiddenError from "../models/errors/forbiddenError";
+import DatabaseError from "../models/errors/databaseError";
+import BadRequestError from "../models/errors/badRequestError";
+import UserError from "../models/errors/userError";
+import UnauthorizedError from "../models/errors/unauthorizedError";
 
 function errorHandler(error: any, _req: Request, res: Response, _next: NextFunction) {
+    let status;
     if (error instanceof ForbiddenError) {
-        res.sendStatus(StatusCodes.FORBIDDEN);
+        status = StatusCodes.FORBIDDEN;
+    }
+    else if (error instanceof BadRequestError) {
+        status = StatusCodes.BAD_REQUEST;
+    }
+    else if (error instanceof DatabaseError) {
+        status = StatusCodes.BAD_REQUEST;
+    }
+    else if (error instanceof UnauthorizedError) {
+        status = StatusCodes.UNAUTHORIZED;
+    }
+    else if (error instanceof UserError) {
+        status = StatusCodes.UNAUTHORIZED;
     }
     else {
-        res.sendStatus(StatusCodes.INTERNAL_SERVER_ERROR);
+        status = StatusCodes.INTERNAL_SERVER_ERROR;
     }
+
+    res.status(status).json({ message: error.message });
 }
 
 
