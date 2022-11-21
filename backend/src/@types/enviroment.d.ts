ad48436897390d4417efd332bd30ee54f483aa2d
JNCJcoder <jorgenettocjunior@gmail.com>
2022-11-21 04:28:16 -0300

    Adicionado variável de ambiente POSTGRES_HOST

--- backend/src/@types/enviroment.d.ts
+++ backend/src/@types/enviroment.d.ts
@@ -5,6 +5,7 @@ declare global {
             PORT?: string;
             PEPPER: string;
             JWT_SECRET: string;
+            POSTGRES_HOST: string;
             POSTGRES_DB: string;
             POSTGRES_USER: string;
             POSTGRES_PASSWORD: string;
