66cafaed6f5e3850b94dd102cde73f3c2f956fd1
JNCJcoder <jorgenettocjunior@gmail.com>
2022-11-21 16:43:51 -0300

    Corrigido problema de importação

--- backend/src/app-data-source.ts
+++ backend/src/app-data-source.ts
@@ -1,14 +1,18 @@
 import { DataSource } from "typeorm"
 
+import User from "./models/User";
+import Account from "./models/Account";
+import Transaction from "./models/Transaction";
+
 const myDataSource = new DataSource({
     type: "postgres",
-    host: "localhost",
-    port: 3306,
+    host: String(process.env.POSTGRES_HOST),
+    port: 5432,
     username: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DB,
-    entities: ["src/models/*.js"],
-    logging: true,
+    entities: [User, Account, Transaction],
+    logging: false,
     synchronize: true,
 });
 
