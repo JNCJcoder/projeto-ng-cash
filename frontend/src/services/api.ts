c50271971546dd3acf4b1c36a7089a1ae543bc61
JNCJcoder <jorgenettocjunior@gmail.com>
2023-02-11 08:11:41 -0300

    FrontEnd - Adicionado env PORT

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 const host = process.env.REACT_APP_BACKEND_HOST || "localhost";
+const port = Number(process.env.REACT_APP_PORT) || 3333;
 
 const api = axios.create({
-    baseURL: `http://${host}:3333`,
+    baseURL: `http://${host}:${port}`,
     validateStatus: () => true
 });
 
--- frontend/src/types/enviroment.d.ts
+++ frontend/src/types/enviroment.d.ts
@@ -3,6 +3,7 @@ declare global {
         interface ProcessEnv {
             NODE_ENV: 'development' | 'production';
             REACT_APP_BACKEND_HOST: string;
+            REACT_APP_PORT: string;
         }
     }
 }
